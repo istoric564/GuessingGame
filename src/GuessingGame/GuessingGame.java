b317396cceb7169104c6403ade0cdabe46fd1476
istoric564 <mrdizzy38@gmail.com>
2021-03-22 19:14:01 +0700

    Необходимо угадать число от 1 до 100

---
+++ src/GuessingGame/GuessingGame.java
@@ -0,0 +1,39 @@
+package GuessingGame;
+
+import javax.swing.*;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        int computerNumber = (int) (Math.random()*100 +1);
+        int userAnswer = 0;
+        int count = 1;
+        
+        while (userAnswer != computerNumber){
+            String response = JOptionPane.showInputDialog(null,
+                    "Введите число от 1 до 100: ", "Угадай число", 3);
+            userAnswer = Integer.parseInt(response);
+            JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
+            count++;
+        }
+    }
+    public static String determineGuess(int userAnswer, int computerNumber, int count){
+        if(userAnswer <=0 || userAnswer > 100){
+            return "Ваше число не в заданном диапазоне";
+        }
+        else if (userAnswer == computerNumber){
+            return "ВЫ УГАДАЛИ!" + "\n" + "Кол-во попыток: " + count;
+        }
+        else if (userAnswer > computerNumber){
+            return "Ваше число больше загаданного. " + "\n" + "Попробуйте снова" + "\n" +
+                    "Кол-во попыток: " + count;
+        }
+        else if (userAnswer < computerNumber){
+            return "Ваше число меньше загаданного. " + "\n" + "Попробуйте снова" + "\n" +
+                    "Кол-во попыток: " + count;
+        }
+        else {
+            return "Ваш ввод некорректен." + "\n" + "Попробуйте снова" + "\n" +
+                    "Кол-во попыток: " + count;
+        }
+    }
+}
